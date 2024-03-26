0d36bd62083af0b95edfb0c0b1245faa7117d293
ElMalditoRey <133232486+ElMalditoRey@users.noreply.github.com>
2024-03-26 17:02:02 -0400

    reference from class

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class gamemanager : MonoBehaviour
 {
+
+    [Header("Global Vars")]
+    public GameObject Player3D;
+    public float timer;
+
+    [Header("UI/UX Vars")]
+    public TextMeshProUGUI timetext;
+
+    public enum GameState
+    {
+        START, PLAYING, END
+    };
+
+    public GameState myGameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myGameState = GameState.START;
+        Player3D.SetActive(false);
     }
 
     // Update is called once per frame
